a17cbd42ab576345a9cd2d2e1ef00e41d84510d7
euler <zxm987626@gmail.com>
2025-05-07 01:19:07 +0800

    delete duplicates

--- List/83.go
+++ List/83.go
@@ -1,6 +1,10 @@
 package List
 
-func DeleteDuplicates(head *ListNode) *ListNode {
+// my func
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head != nil {
+		return nil
+	}
 	dummy := &ListNode{}
 	dummy.Next = head
 	pre := head
@@ -12,5 +16,18 @@ func DeleteDuplicates(head *ListNode) *ListNode {
 		}
 		pre = pre.Next
 	}
+	current.Next = nil
 	return dummy.Next
+}
+
+// leetcode func
+func DeleteDuplicates(head *ListNode) *ListNode {
+	current := head
+	if current != nil {
+		if current.Next.Next != nil && current.Next.Val == current.Val {
+			current.Next = current.Next.Next
+		}
+		current = current.Next
+	}
+	return head
 }
