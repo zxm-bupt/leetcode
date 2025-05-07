21d6b8abe1ae9b8764685e84f1bb8b5be5a903de
euler <zxm987626@gmail.com>
2025-05-07 15:02:55 +0800

    reverseKGroup

---
+++ List/25.go
@@ -0,0 +1,28 @@
+package List
+
+func ReverseKGroup(head *ListNode, k int) *ListNode {
+	dummy := &ListNode{}
+	dummy.Next = head
+	current := dummy
+	if head == nil {
+		return nil
+	}
+	count := 0
+	for head != nil {
+		count++
+		if count == k {
+			temp := head.Next
+			head.Next = nil
+			head = temp
+			current.Next = ResverseList(current.Next)
+			for current.Next != nil {
+				current = current.Next
+			}
+			current.Next = head
+		} else {
+			head = head.Next
+		}
+	}
+
+	return dummy.Next
+}
--- main.go
+++ main.go
@@ -15,7 +15,13 @@ func main() {
 	node.Next.Next = &List.ListNode{
 		Val: 3,
 	}
-	node = List.ResverseList(node)
+	node.Next.Next.Next = &List.ListNode{
+		Val: 4,
+	}
+	node.Next.Next.Next.Next = &List.ListNode{
+		Val: 5,
+	}
+	node = List.ReverseKGroup(node, 2)
 	for node != nil {
 		fmt.Print(node.Val)
 		node = node.Next
