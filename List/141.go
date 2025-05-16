731bfb675e15ce2f519331eab3ed21f22a6ad395
euler <zxm987626@gmail.com>
2025-05-16 18:17:56 +0800

    环形链表

---
+++ List/141.go
@@ -0,0 +1,18 @@
+package List
+
+// 判断链表是否有环
+// 使用快慢指针法
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
